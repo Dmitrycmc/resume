7fbab78733d1bd668018abe2a8a76221827be5fd
Dmitry Lytov <dmitrycmc@gmail.com>
2019-10-19 12:59:18 +0300

    Updates

---
+++ precache-manifest.5f7912e26e28756cfd71867f36cecaf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/static/media/digital-7 (mono).58045dab.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bfa38f7a5bfd0242ac04",
+    "url": "/static/js/main.0cd33468.chunk.js"
+  },
+  {
+    "revision": "1b3b3596f8789cc0ad45",
+    "url": "/static/js/2.9e86e0bd.chunk.js"
+  },
+  {
+    "revision": "bfa38f7a5bfd0242ac04",
+    "url": "/static/css/main.8b4ab62c.chunk.css"
+  },
+  {
+    "revision": "81d8e12854cf84a6e4bd49159afc220c",
+    "url": "/index.html"
+  }
+];
