86c568f54ea213b76d8e43f6f2fc55ab457eb67f
Dmitry Lytov <dmitrycmc@gmail.com>
2019-10-19 19:40:51 +0300

    Added first block background

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import background1 from '../assets/images/background1.jpeg';
+import styled from 'styled-components/macro';
 
-const Landing = () => <div>Hello, resume!</div>;
+const Container = styled.div`
+    background-image: url(${background1});
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+`;
+
+
+const Landing = () => <Container />;
 
 export default Landing;
