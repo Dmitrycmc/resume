a485137b2f1540ae705d08b47f26821c65bb95db
Dmitry Lytov <dmitrycmc@gmail.com>
2019-10-21 06:06:56 +0300

    Progress

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components/macro';
+
+const backgroundCss = css`
+    background-color: rgba(23, 25, 28, 0.9);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
+`;
 
 const Container = styled.ul`
     position: fixed;
@@ -9,33 +14,61 @@ const Container = styled.ul`
     align-items: center;
     box-sizing: border-box;
     width: 100%;
-    height: 40px;
+    height: 70px;
     margin: 0;
     padding: 0 50px 0 0;
 
     color: white;
 
     list-style: none;
+
+    transition: background-color 0.5s, box-shadow 0.5s;
+
+    z-index: 50;
+    ${props => (props.background ? backgroundCss : '')}
 `;
 
 const MenuItem = styled.li`
     display: inline;
     margin: 0 20px;
 
+    font-size: 14px;
+    font-family: Opensans, sans-serif;
+    text-transform: uppercase;
+
     cursor: pointer;
 
     user-select: none;
 `;
 
-const Navigation = () => (
-    <Container>
-        <MenuItem>Старт</MenuItem>
-        <MenuItem>Обо мне</MenuItem>
-        <MenuItem>Навыки</MenuItem>
-        <MenuItem>Образование и опыт</MenuItem>
-        <MenuItem>Примеры</MenuItem>
-        <MenuItem>Контакты</MenuItem>
-    </Container>
-);
+const getBackgroundState = () => {
+    return !!window.pageYOffset;
+};
+
+const Navigation = () => {
+    const [background, setBackground] = useState(getBackgroundState());
+
+    const onScroll = () => {
+        setBackground(getBackgroundState());
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll);
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        };
+    }, []);
+
+    return (
+        <Container background={background}>
+            <MenuItem>Старт</MenuItem>
+            <MenuItem>Обо мне</MenuItem>
+            <MenuItem>Навыки</MenuItem>
+            <MenuItem>Образование и опыт</MenuItem>
+            <MenuItem>Примеры</MenuItem>
+            <MenuItem>Контакты</MenuItem>
+        </Container>
+    );
+};
 
 export default Navigation;
